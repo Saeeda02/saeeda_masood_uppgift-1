6e70bca7053cf270b5a657ff531a6d541fa4e326
Saeeda02 <saeeda.masood@gritacademy.se>
2024-10-30 13:26:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+
+let foo = 0;
+let bar = 0;
+
+
+
+let divide = foo / bar;
+console.log(divide);
+
+if (isNan(divide))
+    console.log("can't divide us!");
+    
+
+
+
+
+/*alert(123)
+document.write("This text comes from an external script.")
+console.log(456)
+
+// 1) variable
+let x // deklarera en variable x
+x = 99
+
+let foo = "hej" // skapar plus + lägger in värde
+foo = "yxgatan 5b"
+
+
+console.log(foo);
+console.log(foo + " är adressen till mig");
+
+
+
+//ctrl r refresh 
+
+//För att deklarera en variabel sedan tilldela ett värde, hur kan syntaxen se ut?
+//För att skapa en behållare sedan lägga ett värde, hur kan koden se ut?
+
+
+
+console.log( 1 + 1);
+console.log(2 + 3 * 4);
+console.log((2 + 3) * 4);
+
+console.log("hej" + 1);
+
+console.log("hej" + 1 +1);
+console.log("hej" + (1 +1));
+
+console.log(1 +1 + "hej");
+
+console.log("hej" + "san");
+
+
+
+
+let buttonDOM = document.createElement("button")
+buttonDOM.innerText = "Klicka här!!"
+document.body.appendChild(buttonDOM)
+
+let inpuDOM = document.createElement("input")
+buttonDOM.innertext = Klicka här!!
+buttonDOM.type = "submit"
+buttonDOM.value = "Klicka här!!"
+document.body.appendChild(buttonDOM)    
+
+*/
